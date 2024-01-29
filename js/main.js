e7217cad0ec01e9df46652b7de61a871ec5c81df
Mbueno992 <mbueno_92@hotmail.com>
2024-01-29 17:05:39 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 //Variables
-const inputName = document.querySelector('.js-inputName');
 const gameOption = document.querySelectorAll('.js-gameOption');
 const playBtn = document.querySelector('.js-playBtn');
 const player = document.querySelector('.js-player');
@@ -9,36 +8,153 @@ const playerSelect = document.querySelector('.js-playerSelect');
 const sheldonSelect = document.querySelector('.js-sheldonSelect');
 const condition = document.querySelector('.js-condition');
 const msg = document.querySelector('.js-msg');
-const rules = document.querySelector('.js-rules');
 const reset = document.querySelector('.js-reset');
+const playerScore = document.querySelector('.js-playerScore');
+const sheldonScore = document.querySelector('.js-sheldonScore');
+
+let user = 0;
+let pc = 0;
 
 const getRandomNumber = () => {
   return Math.ceil(Math.random() * 5);
 };
 const sheldonMove = () => {
+  let sheldonOption = '';
+
   const move = getRandomNumber();
   if (move === 1) {
+    sheldonOption = 'rock';
     sheldonSelect.src = './img/piedra.png';
   } else if (move === 2) {
+    sheldonOption = 'spock';
     sheldonSelect.src = './img/Spock.png';
   } else if (move === 3) {
+    sheldonOption = 'paper';
     sheldonSelect.src = './img/papel.png';
   } else if (move === 4) {
+    sheldonOption = 'scissors';
     sheldonSelect.src = './img/tijeras.png';
   } else {
+    sheldonOption = 'lizard';
     sheldonSelect.src = './img/Lagarto.png';
   }
-};
-
-const getUserName = () => {
-  const user = inputName.value;
-  player.innerHTML = user;
+  return sheldonOption;
 };
 
 const handleSelect = (event) => {
-  const userOption = event.target.src;
-  playerSelect.src = userOption;
+  playerSelect.src = event.target.src;
+  playerSelect.id = event.target.id;
+};
+const gameRules = () => {
+  const userMove = playerSelect.id;
+  if (userMove === '') {
+    msg.innerHTML = 'Selecciona una opción antes de de jugar';
+    playerScore.innerHTML = 'Jugador/a';
+    sheldonScore.innerHTML = 'Sheldon';
+  } else {
+    const sheldon = sheldonMove();
+    if (userMove === sheldon) {
+      msg.innerHTML = 'Ha sido un golpe de suerte, empate.';
+    } else if (userMove === 'rock' && sheldon === 'scissors') {
+      condition.innerHTML = 'Piedra aplasta tijera';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'scissors' && sheldon === 'paper') {
+      condition.innerHTML = 'Tijeras corta a papel';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'paper' && sheldon === 'rock') {
+      condition.innerHTML = 'Papel tapa a piedra';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'rock' && sheldon === 'lizard') {
+      condition.innerHTML = 'Piedra aplasta a lagarto';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'lizard' && sheldon === 'spock') {
+      condition.innerHTML = 'Lagarto envenena a Spock';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'spock' && sheldon === 'scissors') {
+      condition.innerHTML = 'Spock rompe tijeras';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'scissors' && sheldon === 'lizard') {
+      condition.innerHTML = 'Tijera decapita a lagarto';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'lizard' && sheldon === 'paper') {
+      condition.innerHTML = 'Lagarto devora a papel';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'paper' && sheldon === 'spock') {
+      condition.innerHTML = 'Papel desautoriza a Spock';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (userMove === 'spock' && sheldon === 'rock') {
+      condition.innerHTML = 'Spock vaporiza a piedra';
+      msg.innerHTML = '¡Porras, me has ganado!';
+      user++;
+    } else if (sheldon === 'rock' && userMove === 'scissors') {
+      condition.innerHTML = 'Piedra aplasta tijera';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'scissors' && userMove === 'paper') {
+      condition.innerHTML = 'Tijeras corta a papel';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'paper' && userMove === 'rock') {
+      condition.innerHTML = 'Papel tapa a piedra';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'rock' && userMove === 'lizard') {
+      condition.innerHTML = 'Piedra aplasta a lagarto';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'lizard' && userMove === 'spock') {
+      condition.innerHTML = 'Lagarto envenena a Spock';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'spock' && userMove === 'scissors') {
+      condition.innerHTML = 'Spock rompe tijeras';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'scissors' && userMove === 'lizard') {
+      condition.innerHTML = 'Tijera decapita a lagarto';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'lizard' && userMove === 'paper') {
+      condition.innerHTML = 'Lagarto devora a papel';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (sheldon === 'paper' && userMove === 'spock') {
+      condition.innerHTML = 'Papel desautoriza a Spock';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    } else if (userMove === 'spock' && sheldon === 'rock') {
+      condition.innerHTML = 'Spock vaporiza a piedra';
+      msg.innerHTML = '¡La ciencia gana y tú has perdido';
+      pc++;
+    }
+    paintScore();
+  }
+};
+const paintScore = () => {
+  playerScore.innerHTML = `Jugador/a: ${user}`;
+  sheldonScore.innerHTML = `Sheldon: ${pc}`;
 };
 
-inputName.addEventListener('input', getUserName);
+const resetGame = (event) => {
+  event.preventDefault();
+  msg.innerHTML = '¿Listo para la derrota?';
+  playerScore.innerHTML = 'Jugador/a';
+  sheldonScore.innerHTML = 'Sheldon';
+  sheldonSelect.src = '';
+  playerSelect.src = '';
+  playerSelect.id = '';
+  condition.innerHTML = '';
+};
+
 gameOption.forEach((option) => option.addEventListener('click', handleSelect));
+playBtn.addEventListener('click', gameRules);
+reset.addEventListener('click', resetGame);
